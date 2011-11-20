26be29ee77aac82d81631676003f84b17ca86f02
andreas.veithen <andreas.veithen@8a49ac67-29fa-b6ea-cdc4-99157f5378d6>
2011-11-20 12:30:34 +0000

    First working version of the JMX connector plugin.

---
+++ com.googlecode.xm4was.commons/src/com/googlecode/xm4was/commons/AbstractWsComponent.java
@@ -0,0 +1,61 @@
+package com.googlecode.xm4was.commons;
+
+import com.ibm.ws.exception.ComponentDisabledException;
+import com.ibm.ws.exception.ConfigurationError;
+import com.ibm.ws.exception.ConfigurationWarning;
+import com.ibm.ws.exception.RuntimeError;
+import com.ibm.ws.exception.RuntimeWarning;
+import com.ibm.wsspi.runtime.component.WsComponent;
+
+public abstract class AbstractWsComponent implements WsComponent {
+	private String state;
+
+	public final String getState() {
+		return state;
+	}
+
+	public final void initialize(Object config) throws ComponentDisabledException,
+			ConfigurationWarning, ConfigurationError {
+		state = INITIALIZING;
+		try {
+			doInitialize();
+		} catch (Exception ex) {
+			throw new ConfigurationError(ex);
+		}
+		state = INITIALIZED;
+	}
+	
+	protected void doInitialize() throws Exception {
+	}
+	
+	public final void start() throws RuntimeError, RuntimeWarning {
+		state = STARTING;
+		try {
+			doStart();
+		} catch (Exception ex) {
+			throw new RuntimeError(ex);
+		}
+		state = STARTED;
+	}
+
+	protected void doStart() throws Exception {
+	}
+	
+	public final void stop() {
+		state = STOPPING;
+		doStop();
+		state = STOPPED;
+	}
+
+	protected void doStop() {
+	}
+	
+	public final void destroy() {
+		state = DESTROYING;
+		doDestroy();
+		state = DESTROYED;
+	}
+
+	protected void doDestroy() {
+	}
+}
---
+++ com.googlecode.xm4was.jmx/src/com/googlecode/xm4was/jmx/JmxConnector.java
@@ -0,0 +1,73 @@
+package com.googlecode.xm4was.jmx;
+
+import java.io.IOException;
+import java.rmi.NoSuchObjectException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.management.remote.JMXConnectorServer;
+import javax.management.remote.JMXConnectorServerFactory;
+import javax.management.remote.JMXServiceURL;
+import javax.naming.Context;
+
+import com.googlecode.xm4was.commons.AbstractWsComponent;
+import com.ibm.ejs.ras.Tr;
+import com.ibm.ejs.ras.TraceComponent;
+import com.ibm.websphere.management.AdminServiceFactory;
+
+// * Security concerns in http://blogs.oracle.com/lmalventosa/entry/mimicking_the_out_of_the
+// * Thread pool?
+public class JmxConnector extends AbstractWsComponent {
+    private static final TraceComponent TC = Tr.register(JmxConnector.class, "XM4WAS", null);
+    
+    private int port;
+	private Registry registry;
+	private JMXConnectorServer server;
+	
+	public String getName() {
+		return "XM_JmxConnector";
+	}
+
+	@Override
+    protected void doInitialize() throws Exception {
+	    port = 1099;
+    }
+
+    @Override
+	protected void doStart() throws Exception {
+        JMXServiceURL url = new JMXServiceURL("service:jmx:rmi:///jndi/rmi://localhost:" + port + "/jmxrmi");
+        Tr.info(TC, "Starting the JMX connector on " + url);
+		registry = LocateRegistry.createRegistry(port);
+		Map<String,Object> env = new HashMap<String,Object>();
+		env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.rmi.registry.RegistryContextFactory");
+		env.put(Context.PROVIDER_URL, "rmi://server:" + port);
+		// env.put("jmx.remote.x.login.config", "WSLogin");
+
+		// SslRMIClientSocketFactory csf = new SslRMIClientSocketFactory();
+		// SslRMIServerSocketFactory ssf = new SslRMIServerSocketFactory();
+		// env.put(RMIConnectorServer.RMI_CLIENT_SOCKET_FACTORY_ATTRIBUTE, csf);
+		// env.put(RMIConnectorServer.RMI_SERVER_SOCKET_FACTORY_ATTRIBUTE, ssf); 
+		server = JMXConnectorServerFactory.newJMXConnectorServer(url, env,
+				AdminServiceFactory.getMBeanFactory().getMBeanServer());
+		server.start();
+	}
+
+	@Override
+	protected void doStop() {
+		try {
+			server.stop();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try {
+			UnicastRemoteObject.unexportObject(registry, true);
+		} catch (NoSuchObjectException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
