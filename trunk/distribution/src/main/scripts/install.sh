6eaaf898ed0612bac728b849f2fe4cb506d7a5a4
andreas.veithen <andreas.veithen@8a49ac67-29fa-b6ea-cdc4-99157f5378d6>
2012-05-10 20:56:33 +0000

    Added an installation script.

---
+++ trunk/distribution/src/main/scripts/install.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+set -e
+if [ -z "$1" ]; then
+  echo "Usage: $0 <WAS install directory>"
+  exit 1
+fi
+DIST_DIR=$(dirname $0)
+WAS_INSTALL_DIR=$1
+echo "Discovering WAS installation ..."
+if [ ! -d "$WAS_INSTALL_DIR" ]; then
+  echo "$WAS_INSTALL_DIR doesn't exist or is not a directory"
+  exit 1
+fi
+if [ ! -w "$WAS_INSTALL_DIR/plugins" ]; then
+  echo "$WAS_INSTALL_DIR/plugins is not writable"
+  exit 1
+fi
+MANAGE_PROFILES=$WAS_INSTALL_DIR/bin/manageprofiles.sh
+if [ ! -x "$MANAGE_PROFILES" ]; then
+  echo "manageprofiles.sh command not found"
+  exit 1
+fi
+PROFILES=$($MANAGE_PROFILES -listProfiles | sed -e 's/[][,]//g')
+PROFILE_PATHS=
+for PROFILE_NAME in $PROFILES; do
+  PROFILE_PATH=$($MANAGE_PROFILES -getPath -profileName $PROFILE_NAME)
+  if [ -z "$PROFILE_PATHS" ]; then
+    PROFILE_PATHS=$PROFILE_PATH
+  else
+    PROFILE_PATHS="$PROFILE_PATHS $PROFILE_PATH"
+  fi
+done
+echo "  Install directory: $WAS_INSTALL_DIR"
+echo "  Profiles: $PROFILE_PATHS"
+echo "Installing plugins ..."
+rm -f $WAS_INSTALL_DIR/plugins/com.googlecode.xm4was.*.jar
+for PLUGIN in $DIST_DIR/plugins/*.jar; do
+  echo "  $(basename $PLUGIN)"
+  cp $PLUGIN $WAS_INSTALL_DIR/plugins/
+done
+echo "Cleaning OSGi caches ..."
+for PROFILE_PATH in $PROFILE_PATHS; do
+  $PROFILE_PATH/bin/osgiCfgInit.sh
+done
