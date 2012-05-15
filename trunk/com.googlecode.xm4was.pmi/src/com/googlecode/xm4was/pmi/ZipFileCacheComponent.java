5f198e4dfdea43e8bc83ee739be6c451f84cc36d
andreas.veithen <andreas.veithen@8a49ac67-29fa-b6ea-cdc4-99157f5378d6>
2012-05-15 08:54:22 +0000

    Added a PMI module to get statistics about the ZipFileCache in WebSphere.

---
+++ trunk/com.googlecode.xm4was.pmi/src/com/googlecode/xm4was/pmi/ZipFileCacheComponent.java
@@ -0,0 +1,28 @@
+package com.googlecode.xm4was.pmi;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.googlecode.xm4was.commons.AbstractWsComponent;
+import com.ibm.ws.classloader.SinglePathClassProvider;
+import com.ibm.wsspi.pmi.factory.StatsFactory;
+
+public class ZipFileCacheComponent extends AbstractWsComponent {
+    @Override
+    protected void doStart() throws Exception {
+        if (!StatsFactory.isPMIEnabled()) {
+            return;
+        }
+        
+        Field zipFileCacheField = SinglePathClassProvider.class.getDeclaredField("zipFileCache");
+        zipFileCacheField.setAccessible(true);
+        Map<?,?> zipFileCache = (Map<?,?>)zipFileCacheField.get(null);
+        
+        Field modCountField = HashMap.class.getDeclaredField("modCount");
+        modCountField.setAccessible(true);
+        
+        createStatsInstance("ZipFileCacheStats", "/xm4was/ZipFileCacheStats.xml", null,
+                new ZipFileCacheMonitor(zipFileCache, modCountField));
+    }
+}
