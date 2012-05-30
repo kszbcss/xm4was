5c7aa857865f66e839de20b7760024de5cb89193
andreas.veithen <andreas.veithen@8a49ac67-29fa-b6ea-cdc4-99157f5378d6>
2012-05-30 13:29:00 +0000

    created tag for 0.3.0

---
+++ tags/0.3.0/com.googlecode.xm4was.jmx/src/com/googlecode/xm4was/jmx/jrmp/JmxConnector.java
@@ -0,0 +1,110 @@
+package com.googlecode.xm4was.jmx.jrmp;
+
+import java.io.IOException;
+import java.lang.reflect.Proxy;
+import java.rmi.NoSuchObjectException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.management.remote.JMXConnectorServer;
+import javax.management.remote.JMXConnectorServerFactory;
+import javax.management.remote.JMXServiceURL;
+import javax.management.remote.MBeanServerForwarder;
+import javax.naming.Context;
+
+import com.googlecode.xm4was.commons.AbstractWsComponent;
+import com.googlecode.xm4was.commons.TrConstants;
+import com.googlecode.xm4was.jmx.resources.Messages;
+import com.ibm.ejs.ras.Tr;
+import com.ibm.ejs.ras.TraceComponent;
+import com.ibm.websphere.management.AdminServiceFactory;
+import com.ibm.websphere.models.config.ipc.EndPoint;
+import com.ibm.ws.runtime.service.EndPointMgr;
+import com.ibm.ws.security.service.SecurityService;
+import com.ibm.wsspi.runtime.service.WsServiceRegistry;
+
+// * Security concerns in http://blogs.oracle.com/lmalventosa/entry/mimicking_the_out_of_the
+// * Thread pool?
+public class JmxConnector extends AbstractWsComponent {
+    private static final TraceComponent TC = Tr.register(JmxConnector.class, TrConstants.GROUP, Messages.class.getName());
+    
+    private boolean enabled;
+    private int port;
+    private Registry registry;
+    private JMXConnectorServer server;
+    
+    @Override
+    protected void doInitialize() throws Exception {
+        EndPointMgr epMgr = WsServiceRegistry.getService(this, EndPointMgr.class);
+        // We don't use getEndPointInfo, because it only exists in WAS 7.0, but not WAS 6.1
+        EndPoint ep = epMgr.getNodeEndPoints("@").getServerEndPoints("@").getEndPoint("JRMP_CONNECTOR_ADDRESS");
+        if (ep == null) {
+            enabled = false;
+        } else {
+            enabled = true;
+            // TODO: take into account host?
+            port = ep.getPort();
+        }
+    }
+
+    @Override
+    protected void doStart() throws Exception {
+        if (enabled) {
+            JMXServiceURL url = new JMXServiceURL("service:jmx:rmi:///jndi/rmi://localhost:" + port + "/jmxrmi");
+            Tr.info(TC, Messages._0001I, new Object[] { url.toString() });
+            SecurityService securityService = WsServiceRegistry.getService(this, SecurityService.class);
+            boolean securityEnabled = securityService.isSecurityEnabled();
+            Tr.info(TC, securityEnabled ? Messages._0003I : Messages._0004I);
+            registry = LocateRegistry.createRegistry(port);
+            addStopAction(new Runnable() {
+                public void run() {
+                    try {
+                        UnicastRemoteObject.unexportObject(registry, true);
+                    } catch (NoSuchObjectException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            });
+            Map<String,Object> env = new HashMap<String,Object>();
+            env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.rmi.registry.RegistryContextFactory");
+            env.put(Context.PROVIDER_URL, "rmi://server:" + port);
+            if (securityEnabled) {
+                env.put(JMXConnectorServer.AUTHENTICATOR, new WebSphereJMXAuthenticator());
+            }
+    
+            // TODO: enable SSL
+            // * WebSphere API to get socket factory: JSSEHelper.getSSLServerSocketFactory
+            // * Also use SSLConfigChangeListener to reconfigure the socket when config changes
+            // SslRMIClientSocketFactory csf = new SslRMIClientSocketFactory();
+            // SslRMIServerSocketFactory ssf = new SslRMIServerSocketFactory();
+            // env.put(RMIConnectorServer.RMI_CLIENT_SOCKET_FACTORY_ATTRIBUTE, csf);
+            // env.put(RMIConnectorServer.RMI_SERVER_SOCKET_FACTORY_ATTRIBUTE, ssf);
+            if (TC.isDebugEnabled()) {
+                Tr.debug(TC, "Creating JMX connector with env={0}", env);
+            }
+            server = JMXConnectorServerFactory.newJMXConnectorServer(url, env,
+                    AdminServiceFactory.getMBeanFactory().getMBeanServer());
+            if (securityEnabled) {
+                server.setMBeanServerForwarder((MBeanServerForwarder)Proxy.newProxyInstance(JmxConnector.class.getClassLoader(),
+                        new Class<?>[] { MBeanServerForwarder.class }, new WebSphereMBeanServerInvocationHandler()));
+            }
+            server.start();
+            addStopAction(new Runnable() {
+                public void run() {
+                    try {
+                        server.stop();
+                    } catch (IOException e1) {
+                        // TODO Auto-generated catch block
+                        e1.printStackTrace();
+                    }
+                }
+            });
+        } else {
+            Tr.info(TC, Messages._0002I);
+        }
+    }
+}
---
+++ tags/0.3.0/com.googlecode.xm4was.pmi/src/com/googlecode/xm4was/pmi/zfc/ZipFileCacheMonitor.java
@@ -0,0 +1,46 @@
+package com.googlecode.xm4was.pmi.zfc;
+
+import java.lang.reflect.Field;
+import java.util.Map;
+
+import com.ibm.wsspi.pmi.factory.StatisticActions;
+import com.ibm.wsspi.pmi.stat.SPICountStatistic;
+import com.ibm.wsspi.pmi.stat.SPIStatistic;
+
+public class ZipFileCacheMonitor extends StatisticActions {
+    private static final int MOD_COUNT = 1;
+    
+    private final Map<?,?> zipFileCache;
+    private final Field modCountField;
+    private SPICountStatistic modCount;
+
+    public ZipFileCacheMonitor(Map<?,?> zipFileCache, Field modCountField) {
+        this.zipFileCache = zipFileCache;
+        this.modCountField = modCountField;
+    }
+
+    @Override
+    public void statisticCreated(SPIStatistic statistic) {
+        switch (statistic.getId()) {
+            case MOD_COUNT:
+                modCount = (SPICountStatistic)statistic;
+                break;
+        }
+    }
+
+    @Override
+    public void updateStatisticOnRequest(int dataId) {
+        switch (dataId) {
+            case MOD_COUNT:
+                synchronized (zipFileCache) {
+                    try {
+                        modCount.setCount(modCountField.getInt(zipFileCache));
+                    } catch (Exception ex) {
+                        // There is no reason why we would get here...
+                        throw new RuntimeException(ex);
+                    }
+                }
+                break;
+        }
+    }
+}
