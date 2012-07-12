fa08c5140ce8673d2fd200565f6331044aac57df
andreas.veithen <andreas.veithen@8a49ac67-29fa-b6ea-cdc4-99157f5378d6>
2012-07-12 13:45:13 +0000

    Added code that attempts to parse stacktraces logged via WebSphere's Tr API and to convert them to the format used by XM4WAS.

--- com.googlecode.xm4was.logging.tests/src/com/googlecode/xm4was/logging/ExceptionUtilTest.java
+++ com.googlecode.xm4was.logging.tests/src/com/googlecode/xm4was/logging/ExceptionUtilTest.java
@@ -1,5 +1,10 @@
 package com.googlecode.xm4was.logging;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Proxy;
 
@@ -7,12 +12,12 @@ import org.junit.Test;
 
 public class ExceptionUtilTest {
     @Test
-    public void testSimple() throws Exception {
+    public void testFormatSimple() throws Exception {
         try {
             new MyClass().method3();
         } catch (RuntimeException ex) {
             LineAppenderImpl appender = new LineAppenderImpl();
-            ExceptionUtil.formatStackTrace(ex, appender);
+            ExceptionUtil.formatStackTrace(ExceptionUtil.process(ex), appender);
             appender.assertLine("java\\.lang\\.RuntimeException: Test");
             appender.assertLine(" \\| com\\.googlecode\\.xm4was\\.logging\\.MyClass\\.method1\\([0-9]+\\)");
             appender.assertLine(" \\| com\\.googlecode\\.xm4was\\.logging\\.MyClass\\.method2\\([0-9]+\\)");
@@ -22,40 +27,74 @@ public class ExceptionUtilTest {
             appender.assertLine(" \\| com\\.googlecode\\.xm4was\\.logging\\.MyClass\\.method3\\([0-9]+\\)");
             appender.assertLine("Wrapped by: java\\.lang\\.RuntimeException: Another wrapper");
             appender.assertLine(" \\+ com\\.googlecode\\.xm4was\\.logging\\.MyClass\\.method3\\([0-9]+\\)");
-            appender.assertLine(" \\| com\\.googlecode\\.xm4was\\.logging\\.ExceptionUtilTest\\.testSimple\\([0-9]+\\)");
+            appender.assertLine(" \\| com\\.googlecode\\.xm4was\\.logging\\.ExceptionUtilTest\\.testFormatSimple\\([0-9]+\\)");
         }
     }
     
     @Test
-    public void testReflectiveInvocation() throws Exception {
+    public void testFormatReflectiveInvocation() throws Exception {
         try {
             MyClass.class.getDeclaredMethod("method1").invoke(new MyClass());
         } catch (InvocationTargetException ex) {
             LineAppenderImpl appender = new LineAppenderImpl();
-            ExceptionUtil.formatStackTrace(ex, appender);
+            ExceptionUtil.formatStackTrace(ExceptionUtil.process(ex), appender);
             appender.assertLine("java\\.lang\\.RuntimeException: Test");
             appender.assertLine(" \\| com\\.googlecode\\.xm4was\\.logging\\.MyClass\\.method1\\([0-9]+\\)");
             appender.assertLine("Wrapped by: java\\.lang\\.reflect\\.InvocationTargetException");
             // Normally there would be a couple of additional frames here, but they are suppressed by
             // the formatter.
             appender.assertLine(" \\| java\\.lang\\.reflect\\.Method\\.invoke\\([0-9]+\\)");
-            appender.assertLine(" \\| com\\.googlecode\\.xm4was\\.logging\\.ExceptionUtilTest\\.testReflectiveInvocation\\([0-9]+\\)");
+            appender.assertLine(" \\| com\\.googlecode\\.xm4was\\.logging\\.ExceptionUtilTest\\.testFormatReflectiveInvocation\\([0-9]+\\)");
         }
     }
     
     @Test
-    public void testProxy() throws Exception {
+    public void testFormatProxy() throws Exception {
         try {
             DummyInterface proxy = (DummyInterface)Proxy.newProxyInstance(ExceptionUtilTest.class.getClassLoader(),
                     new Class<?>[] { DummyInterface.class }, new DummyInvocationHandler());
             proxy.throwException();
         } catch (RuntimeException ex) {
             LineAppenderImpl appender = new LineAppenderImpl();
-            ExceptionUtil.formatStackTrace(ex, appender);
+            ExceptionUtil.formatStackTrace(ExceptionUtil.process(ex), appender);
             appender.assertLine("java\\.lang\\.RuntimeException: Test exception");
             appender.assertLine(" \\| com\\.googlecode\\.xm4was\\.logging\\.DummyInvocationHandler\\.invoke\\([0-9]+\\)");
             appender.assertLine(" \\| \\[proxy\\]\\.throwException");
-            appender.assertLine(" \\| com\\.googlecode\\.xm4was\\.logging\\.ExceptionUtilTest\\.testProxy\\([0-9]+\\)");
+            appender.assertLine(" \\| com\\.googlecode\\.xm4was\\.logging\\.ExceptionUtilTest\\.testFormatProxy\\([0-9]+\\)");
+        }
+    }
+    
+    @Test
+    public void testParse() throws Exception {
+        try {
+            new MyClass().method3();
+        } catch (RuntimeException ex) {
+            StringWriter sw = new StringWriter();
+            PrintWriter out = new PrintWriter(sw, false);
+            ex.printStackTrace(out);
+            out.flush();
+            ThrowableInfo[] throwables = ExceptionUtil.parse(sw.toString());
+            assertNotNull(throwables);
+            ThrowableInfo[] expected = ExceptionUtil.process(ex);
+            assertEquals(expected.length, throwables.length);
+            for (int i=0; i<expected.length; i++) {
+                assertEquals(expected[i].getMessage(), throwables[i].getMessage());
+                StackTraceElement[] stackTrace = throwables[i].getStackTrace();
+                StackTraceElement[] expectedStackTrace = expected[i].getStackTrace();
+                assertEquals(expectedStackTrace.length, stackTrace.length);
+                for (int j=0; j<expectedStackTrace.length; j++) {
+                    // The equals method of StackTraceElement doesn't work here because the file name
+                    // is lost if line==-2 (native method)
+                    StackTraceElement frame = stackTrace[j];
+                    StackTraceElement expectedFrame = expectedStackTrace[j];
+                    assertEquals(expectedFrame.getClassName(), frame.getClassName());
+                    assertEquals(expectedFrame.getMethodName(), frame.getMethodName());
+                    assertEquals(expectedFrame.getLineNumber(), frame.getLineNumber());
+                    if (expectedFrame.getLineNumber() > 0) {
+                        assertEquals(expectedFrame.getFileName(), frame.getFileName());
+                    }
+                }
+            }
         }
     }
 }
