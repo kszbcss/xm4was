c449c658d3cf472cfca793aa72050087e9ed8a0e
andreas.veithen <andreas.veithen@8a49ac67-29fa-b6ea-cdc4-99157f5378d6>
2012-07-24 14:32:34 +0000

    Avoid spawning a process to get the kernel page size.

--- com.googlecode.xm4was.pmi/src/com/googlecode/xm4was/pmi/proc/ProcStatsComponent.java
+++ com.googlecode.xm4was.pmi/src/com/googlecode/xm4was/pmi/proc/ProcStatsComponent.java
@@ -1,8 +1,6 @@
 package com.googlecode.xm4was.pmi.proc;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.InputStreamReader;
 
 import com.googlecode.xm4was.commons.AbstractWsComponent;
 import com.googlecode.xm4was.commons.TrConstants;
@@ -40,25 +38,6 @@ public class ProcStatsComponent extends AbstractWsComponent {
             Tr.error(TC, Messages._0102E, statmFile);
             return;
         }
-        int pageSize;
-        try {
-            // There seems to be no way to get the pagesize from /proc
-            Process getconf = Runtime.getRuntime().exec(new String[] { "/usr/bin/getconf", "PAGESIZE"});
-            getconf.getOutputStream().close();
-            getconf.getErrorStream().close();
-            BufferedReader in = new BufferedReader(new InputStreamReader(getconf.getInputStream()));
-            try {
-                pageSize = Integer.parseInt(in.readLine());
-            } finally {
-                in.close();
-            }
-            if (TC.isDebugEnabled()) {
-                Tr.debug(TC, "Page size is {0} bytes", pageSize);
-            }
-        } catch (Throwable ex) {
-            Tr.error(TC, Messages._0103E, ex);
-            return;
-        }
-        createStatsInstance("ProcStats", "/com/googlecode/xm4was/pmi/ProcStats.xml", null, new ProcStatsCollector(fdDir, statmFile, pageSize));
+        createStatsInstance("ProcStats", "/com/googlecode/xm4was/pmi/ProcStats.xml", null, new ProcStatsCollector(fdDir, statmFile));
     }
 }
--- com.googlecode.xm4was.pmi/src/com/googlecode/xm4was/pmi/resources/Messages.java
+++ com.googlecode.xm4was.pmi/src/com/googlecode/xm4was/pmi/resources/Messages.java
@@ -8,13 +8,11 @@ public class Messages extends ListResourceBundle {
     // Messages related to com.googlecode.xm4was.pmi.proc
     public static final String _0101I = "0101I";
     public static final String _0102E = "0102E";
-    public static final String _0103E = "0103E";
     
     private static final Object[][] contents = {
         { _0001E, "XMPMI0001E: Unable to update statistic {0}\n{1}" },
         { _0101I, "XMPMI0101I: The /proc filesystem doesn't exist on this platform" },
         { _0102E, "XMPMI0102E: {0} doesn't exist" },
-        { _0103E, "XMPMI0103E: Failed to determine page size:\n{0}" },
     };
 
     @Override
