4536888afef478b6b24f0ae6933c128840529637
andreas.veithen <andreas.veithen@8a49ac67-29fa-b6ea-cdc4-99157f5378d6>
2012-11-13 07:55:46 +0000

    Created a branch to work on 0.3.1.

--- com.googlecode.xm4was.commons/src/com/googlecode/xm4was/commons/jmx/ManagementService.java
+++
@@ -1,9 +0,0 @@
-package com.googlecode.xm4was.commons.jmx;
-
-import javax.management.MBeanServer;
-
-public interface ManagementService {
-    MBeanServer getMBeanServer();
-    MBeanServer getRawMBeanServer();
-    Authorizer getAuthorizer();
-}
--- com.googlecode.xm4was.commons/src/com/googlecode/xm4was/commons/osgi/Injector.java
+++
@@ -1,6 +0,0 @@
-package com.googlecode.xm4was.commons.osgi;
-
-interface Injector {
-    void open();
-    void close();
-}
