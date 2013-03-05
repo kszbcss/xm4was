dd145c6380e58f3627742234159bbcf9ff4c7047
andreas.veithen <andreas.veithen@8a49ac67-29fa-b6ea-cdc4-99157f5378d6>
2013-03-05 08:37:14 +0000

    Only check for @MBean and @PMIEnabled annotations on services registered by bundles managed by XM4WAS.

--- com.googlecode.xm4was.commons/src/com/googlecode/xm4was/commons/osgi/impl/ManagedBundle.java
+++ com.googlecode.xm4was.commons/src/com/googlecode/xm4was/commons/osgi/impl/ManagedBundle.java
@@ -3,9 +3,21 @@ package com.googlecode.xm4was.commons.osgi.impl;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.osgi.framework.Bundle;
+
 final class ManagedBundle {
     private final List<LifecycleManager> components = new ArrayList<LifecycleManager>();
     
+    private final Bundle bundle;
+    
+    ManagedBundle(Bundle bundle) {
+        this.bundle = bundle;
+    }
+
+    Bundle getBundle() {
+        return bundle;
+    }
+
     void addComponent(LifecycleManager component) {
         components.add(component);
     }
