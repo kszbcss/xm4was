bb535859c8279f8460c3c8f577fe7c86f602338e
andreas.veithen <andreas.veithen@8a49ac67-29fa-b6ea-cdc4-99157f5378d6>
2013-05-07 14:02:57 +0000

    (Partially) fixed an issue with WAS 8.5 and improved logging.

--- trunk/com.googlecode.xm4was.commons/src/com/googlecode/xm4was/commons/osgi/impl/ManagedBundle.java
+++ trunk/com.googlecode.xm4was.commons/src/com/googlecode/xm4was/commons/osgi/impl/ManagedBundle.java
@@ -5,7 +5,14 @@ import java.util.List;
 
 import org.osgi.framework.Bundle;
 
+import com.googlecode.xm4was.commons.TrConstants;
+import com.googlecode.xm4was.commons.resources.Messages;
+import com.ibm.ejs.ras.Tr;
+import com.ibm.ejs.ras.TraceComponent;
+
 final class ManagedBundle {
+    private static final TraceComponent TC = Tr.register(ManagedBundle.class, TrConstants.GROUP, Messages.class.getName());
+    
     private final List<LifecycleManager> components = new ArrayList<LifecycleManager>();
     
     private final Bundle bundle;
@@ -24,12 +31,18 @@ final class ManagedBundle {
     
     void startComponents() {
         for (LifecycleManager component : components) {
+            if (TC.isDebugEnabled()) {
+                Tr.debug(TC, "Starting component {0}", component);
+            }
             component.start();
         }
     }
     
     void stopComponents() {
         for (LifecycleManager component : components) {
+            if (TC.isDebugEnabled()) {
+                Tr.debug(TC, "Stopping component {0}", component);
+            }
             component.stop();
         }
     }
