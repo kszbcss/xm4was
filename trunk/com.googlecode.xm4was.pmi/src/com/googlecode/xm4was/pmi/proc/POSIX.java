c8e20bf32cce968a7eef449e96e5df92f3d15158
andreas.veithen <andreas.veithen@8a49ac67-29fa-b6ea-cdc4-99157f5378d6>
2013-10-22 12:17:31 +0000

    Restore Windows compatibility: moved the POSIX stuff to a separate bundle that only gets activated on supported POSIX compatible systems (i.e. Linux for the moment).

--- trunk/com.googlecode.xm4was.pmi/src/com/googlecode/xm4was/pmi/proc/POSIX.java
+++
@@ -1,7 +0,0 @@
-package com.googlecode.xm4was.pmi.proc;
-
-import com.sun.jna.Library;
-
-public interface POSIX extends Library {
-    int getpagesize();
-}
--- trunk/com.googlecode.xm4was.pmi/src/com/googlecode/xm4was/pmi/resources/Messages.java
+++ trunk/com.googlecode.xm4was.pmi/src/com/googlecode/xm4was/pmi/resources/Messages.java
@@ -4,12 +4,12 @@ import java.util.ListResourceBundle;
 
 public class Messages extends ListResourceBundle {
     // Messages related to com.googlecode.xm4was.pmi.proc
-    public static final String _0101I = "0101I";
+    public static final String _0101W = "0101W";
     public static final String _0102E = "0102E";
     public static final String _0103W = "0103W";
     
     private static final Object[][] contents = {
-        { _0101I, "XMPMI0101I: The /proc filesystem doesn't exist on this platform" },
+        { _0101W, "XMPMI0101W: The /proc filesystem doesn't exist on this platform" },
         { _0102E, "XMPMI0102E: {0} doesn't exist" },
         { _0103W, "XMPMI0103W: Unable to determine kernel page size; using default value ({1}). Exception was:\n{0}" },
     };
