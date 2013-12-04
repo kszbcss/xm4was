195ba372dfef1270ddd8c1baf64568a114b58a5a
andreas.veithen <andreas.veithen@8a49ac67-29fa-b6ea-cdc4-99157f5378d6>
2013-12-04 09:44:39 +0000

    Added a change missing in the previous commit.

--- trunk/com.googlecode.xm4was.pmi/src/com/googlecode/xm4was/pmi/zfc/ZipFileCacheMonitor.java
+++ trunk/com.googlecode.xm4was.pmi/src/com/googlecode/xm4was/pmi/zfc/ZipFileCacheMonitor.java
@@ -3,14 +3,13 @@ package com.googlecode.xm4was.pmi.zfc;
 import com.github.veithen.rbeans.RBeanFactory;
 import com.googlecode.xm4was.commons.osgi.annotations.Services;
 import com.googlecode.xm4was.commons.rbeans.HashMapRBean;
-import com.ibm.ws.classloader.SinglePathClassProvider;
 
 @Services(ZipFileCacheMonitorMBean.class)
 public class ZipFileCacheMonitor implements ZipFileCacheMonitorMBean {
     private final HashMapRBean<?,?> zipFileCache;
 
     public ZipFileCacheMonitor() throws Exception {
-        zipFileCache = (HashMapRBean<?,?>)new RBeanFactory(SinglePathClassProvider.class).createRBean(SinglePathClassProviderRBean.class).getZipFileCache();
+        zipFileCache = (HashMapRBean<?,?>)new RBeanFactory(SinglePathClassProviderRBean.class).createRBean(SinglePathClassProviderRBean.class).getZipFileCache();
     }
 
     public int getModCount() {
