5469301deb81d0ade10030712c8390a5cfd1b541
petervandenbosch0@gmail.com <petervandenbosch0@gmail.com@8a49ac67-29fa-b6ea-cdc4-99157f5378d6>
2014-09-23 08:05:20 +0000

    IBM JRE 7.0.7 compatibility fix

--- trunk/com.googlecode.xm4was.threadmon/src/com/googlecode/xm4was/threadmon/impl/ThreadRBean.java
+++ trunk/com.googlecode.xm4was.threadmon/src/com/googlecode/xm4was/threadmon/impl/ThreadRBean.java
@@ -6,6 +6,7 @@ import com.github.veithen.rbeans.TargetClass;
 
 @TargetClass(Thread.class)
 public interface ThreadRBean extends RBean {
-    @Accessor(name={"accessControlContext"})
+	// inheritedAccessControlContext is the name of the attribute in IBM Java >= 7.0.7
+	@Accessor(name = { "inheritedAccessControlContext", "accessControlContext" })
     AccessControlContextRBean getAccessControlContext();
 }
